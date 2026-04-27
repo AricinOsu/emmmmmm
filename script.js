ee53137d2b9483f6577ecd912c6362488368a530
AricinOsu <97602886+AricinOsu@users.noreply.github.com>
2026-04-27 14:02:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
+    "https://media.tenor.com/ZB9kXOjfhasAAAAi/cat-kitty.gif",    // 0 normal
+    "https://media.tenor.com/FJtRi5UXT-gAAAAi/pusheen-pusheen-sad.gif",  // 1 confused
+    "https://media1.tenor.com/m/rKv4NbObs8EAAAAC/kinda-lazy-pusheen.gif",             // 2 pleading
+    "https://tenor.com/vi/view/dejected-sad-down-pusheen-cute-gif-12694700",             // 3 sad
+    "https://media.tenor.com/2sRRSDtZRgoAAAAi/pushin.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
